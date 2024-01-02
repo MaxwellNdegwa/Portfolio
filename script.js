c1930262cdf7453275573230ec926c982a81204a
MaxwellNdegwa <ndegwamaxwell07@gmail.com>
2024-01-02 18:57:25 +0300

    Adding Skills in Typed js

--- script.js
+++ script.js
@@ -40,8 +40,19 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200
+    delay: 100
  });
 
  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .about-img,.contact form', { origin: 'bottom' });
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+//  typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Graphic Designer', 'Project Manager'],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
